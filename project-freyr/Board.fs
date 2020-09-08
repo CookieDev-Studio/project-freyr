bf568fc13c15c89164c0f4f988a88169cd9a6d64
Cameron <cameronproulx@outlook.com>
2020-09-08 15:13:21 -0400

    first iteration of scrolling

--- project-freyr/Board.fs
+++ project-freyr/Board.fs
@@ -1,16 +1,45 @@
-﻿module Board
+﻿namespace World
 
 open Entities
 open System
+open Microsoft.Xna.Framework
 
-let tiles = 
-    Array2D.init 30 30 
-        (fun _ _ ->
-            let random = Random()
-            let number = random.Next(3)
-            match number with
-            | 1 -> Entities.watter                                                                                                    
-            | 2 -> Entities.grass 
-            | _ -> Entities.stone)
+type Chunk =
+    { root : int * int
+      tiles : Entity[,] }
+type World =
+    { tiles : Entity[,] }
 
-let size = tiles.GetUpperBound 0
+module WorldOperations =
+
+    let generateWorld worldSize =
+        let generatedTiles =
+            Array2D.init worldSize worldSize
+                ( fun _ _ ->
+                    let random = Random()
+                    let number = random.Next(3)
+                    match number with
+                    | 1 -> Entities.watter                                                                                                    
+                    | 2 -> Entities.grass 
+                    | _ -> Entities.stone )
+
+        { tiles = generatedTiles }
+
+    let getChunk chunkSize (world : World) (rootY, rootX) =
+        let createChunk rootY rootX = 
+            { root = (rootY, rootX)
+              tiles = Array2D.init chunkSize chunkSize 
+                            ( fun y x -> 
+                              world.tiles.[rootY + y, rootX + x] ) }
+
+        let worldSize = world.tiles.GetUpperBound 0
+        if rootY + chunkSize > worldSize then
+            createChunk (rootY - (rootY + chunkSize - worldSize)) rootX
+        elif rootY < 0 then
+            createChunk (rootY + 1) rootX
+        elif rootX + chunkSize > worldSize then
+            createChunk rootY (rootX - (rootX + chunkSize - worldSize))
+        elif rootX < 0 then
+            createChunk rootY (rootX + 1)
+        else
+            createChunk rootY rootX
--- project-freyr/Game.fs
+++ project-freyr/Game.fs
@@ -2,6 +2,8 @@
 
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+open World
 
 type ProjectFreyr () as x =
     inherit Game()
@@ -11,7 +13,16 @@ type ProjectFreyr () as x =
     let mutable spriteBatch = null
     let mutable consolas = null
 
+    let worldSize = 1000
+    let chunkSize = 35
+
+    let world = WorldOperations.generateWorld worldSize
+    let moveChunk = WorldOperations.getChunk chunkSize world
+
+    let mutable chunk = moveChunk (0, 0) 
+
     override x.Initialize() =
+        x.IsMouseVisible <- true
         base.Initialize()
         ()
 
@@ -21,18 +32,35 @@ type ProjectFreyr () as x =
         ()
 
     override x.Update (gameTime) =
+        let keys = Keyboard.GetState()
+
+        if keys.IsKeyDown(Keys.S) || keys.IsKeyDown(Keys.Down) then
+            let (Y,X) = chunk.root
+            chunk <- moveChunk(Y + 1, X)
+
+        if keys.IsKeyDown(Keys.W) || keys.IsKeyDown(Keys.Up) then
+            let (Y,X) = chunk.root
+            chunk <- moveChunk(Y - 1, X)
+
+        if keys.IsKeyDown(Keys.D) || keys.IsKeyDown(Keys.Right) then
+            let (Y,X) = chunk.root
+            chunk <- moveChunk(Y, X + 1)
+            
+        if keys.IsKeyDown(Keys.A) || keys.IsKeyDown(Keys.Left) then
+            let (Y,X) = chunk.root
+            chunk <- moveChunk(Y, X - 1)
         ()
 
     override x.Draw (gameTime) =
         x.GraphicsDevice.Clear Color.Black
         spriteBatch.Begin()
-        Board.tiles |> Array2D.iteri (fun y x entity ->
+        chunk.tiles |> Array2D.iteri (fun y x entity ->
             let tileDistance = graphics.PreferredBackBufferHeight |> float32
 
             spriteBatch.DrawString(
                 consolas,
                 entity.character.ToString(),
-                new Vector2( float32 x, float32 y ) * tileDistance / (Board.size |> float32),
+                new Vector2( float32 x, float32 y ) * tileDistance / (chunkSize |> float32),
                 entity.colour))
  
         spriteBatch.End()
